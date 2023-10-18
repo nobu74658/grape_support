c0b97f068babf702999470fec060dc4cf9cff0d7
nobu74658 <hiroki-pon@outlook.jp>
2023-10-19 08:49:25 +0900

    コンポーネント

---
+++ lib/primary/bottom_elevated_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:grape_support/utils/extension/context.dart';
+import 'package:grape_support/utils/padding.dart';
+
+class BottomElevatedButton extends StatelessWidget {
+  const BottomElevatedButton({
+    required this.onPressed,
+    required this.child,
+    super.key,
+  });
+
+  final VoidCallback onPressed;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.fromLTRB(
+          PaddingStyle.p16,
+          PaddingStyle.p8,
+          PaddingStyle.p16,
+          PaddingStyle.p8 + context.bottomSafeArea,
+        ),
+        child: ElevatedButton(
+          onPressed: onPressed,
+          child: child,
+        ),
+      );
+}
